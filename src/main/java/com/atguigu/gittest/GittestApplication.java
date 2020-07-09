cafcadd20e8c478ee777c6f1472ac0df7d9d0542
jbz_gbl <jbz_gbl@qq.com>
2020-07-09 14:21:38 +0800

    test idea git

---
+++ src/main/java/com/atguigu/gittest/GittestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestApplication.class, args);
+    }
+
+}
